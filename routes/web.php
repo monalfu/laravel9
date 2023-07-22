0e42e31f6978d777ac2707e06cc8fb2bec2f0231
Montse Alguacil <monalfu.developer@gmail.com>
2023-07-22 18:01:20 +0200

    PANEL ADMINISTRATIVO

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController; // dirección del controlador
+use App\Http\Controllers\PostController;
 
 Route::controller(PageController::class)->group(function() {
     Route::get('/', 'home')
@@ -24,5 +25,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('posts', PostController::class)->except('show'); // except para eliminar una ruta
 
 require __DIR__.'/auth.php';
