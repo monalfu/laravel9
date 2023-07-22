25877c316157bd0adf3e7b602025c450429c5a61
Montse Alguacil <monalfu.developer@gmail.com>
2023-07-22 21:07:22 +0200

    Listado

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index() {
-        return view('posts.index');
+        // desarrollar la consulta (orden descendete y paginador)
+        return view('posts.index', [
+            'posts' => Post::latest()->paginate()
+        ]);
     }
 }
