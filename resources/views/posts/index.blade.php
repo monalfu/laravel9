9d193331191cf7c182dd85b2a3be03abcf5808aa
Montse Alguacil <monalfu.developer@gmail.com>
2023-07-23 20:03:34 +0200

    DELETE y configurar tailwind

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -16,7 +16,15 @@
                             <td class="px-6 py-4">
                                 <a href="" class="text-indigo-600">Editar</a>
                             </td>
-                            <td class="px-6 py-4">Eliminar</td>
+                            <td class="px-6 py-4">
+                                <form action="{{ route('posts.destroy', $post) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+
+                                <input type="submit" value="Eliminar" class="bg-red-800 text-white rounded px-4 py-2" onclick="return confirm('Desea eliminar?')">
+
+                                </form>
+                            </td>
                         </tr>
                         @endforeach
                     </table>
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Proyecto web</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
 </head>
 <body>
     <p>
